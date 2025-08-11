9c5d6490da875dcb1ad35a0aaa32f0dd75e64dd9
NCl3 <3300236038@qq.com>
2025-08-11 14:48:51 +0800

    active f loop need debug

--- src/align.cpp
+++ src/align.cpp
@@ -145,8 +145,7 @@ std::vector<res_t> POA_SIMD_ORIGIN(para_t* para, const graph& DAG, const std::st
   auto start1 = std::chrono::high_resolution_clock::now();
 
   int n = DAG.node.size(), m = _seq.size();
-  std::string seq = _seq;
-  assert(seq[0] == para->m - 1);
+  assert(_seq[0] == para->m - 1);
   const std::vector<node_t>& node = DAG.node;const std::vector<int>& rank = DAG.rank;
   std::vector<int> mat = para->mat;
   int match = para->match, mismatch = para->mismatch, para_m = para->m, e1 = para->gap_ext1, o1 = para->gap_open1 + e1;
@@ -154,10 +153,16 @@ std::vector<res_t> POA_SIMD_ORIGIN(para_t* para, const graph& DAG, const std::st
   // std::cout << mat << " " << mis << " " << o1 << " " << e1 << "\n";
   int reg_size = SIMDTotalBytes / sizeof(int); // number of int in reg
   int col_size = (m + 1 + reg_size - 1) / reg_size * reg_size;
-  seq.append(col_size - m, char26_table['N']);
-  // std::cerr << seq.size() << " " << col_size << "\n";
-  assert(col_size % reg_size == 0);
   const size_t mtx_size = n * col_size;
+  assert(col_size % reg_size == 0);
+  int block_num = col_size / reg_size; // block_num
+  std::string seq;seq.resize(col_size);
+  for (int i = 0; i < col_size; i++) {
+    // if (i < m) seq[(i % block_size) * reg_size + (i / block_size)] = _seq[i];
+    // else seq[(i % block_size) * reg_size + (i / block_size)] = char26_table['N'];
+    seq[(i % block_num) * reg_size + (i / block_num)] = i < m ? _seq[i] : char26_table['N'];
+  }
+  // std::cerr << seq.size() << " " << col_size << "\n";
   void* buff = nullptr;
   // ===== 2. 对齐内存分配 =====
   // if ((ret = posix_memalign(&buff, SIMDTotalBytes, 3 * mtx_size * sizeof(int))) != 0) {
@@ -176,6 +181,7 @@ std::vector<res_t> POA_SIMD_ORIGIN(para_t* para, const graph& DAG, const std::st
   auto end1 = std::chrono::high_resolution_clock::now();
   total_part1 += std::chrono::duration_cast<std::chrono::microseconds>(end1 - start1);
   int tot = 0;
+  int offset = (block_num - 1) * reg_size;
   for (int i = 1; i < n; i++) {
     const node_t& cur = node[rank[i]];
     int* M_i = M + i * col_size;
@@ -186,54 +192,78 @@ std::vector<res_t> POA_SIMD_ORIGIN(para_t* para, const graph& DAG, const std::st
       int p = node[cur.in[k]].rank; // rank
       const node_t& pre = node[cur.in[k]];
       reg PRE_BASE = _mm256_set1_epi32(pre.base);
-      int prev = NEG_INF;
-      char prech = char26_table['N'];
-      for (int j = 0; j < col_size; j += reg_size) { // SIMD
-        reg Mij = k == 0 ? Neg_inf : _mm256_load_si256((reg*)(M_i + j));
-        reg Dij = k == 0 ? Neg_inf : _mm256_load_si256((reg*)(D_i + j));
-        reg SEQ; // seq[j - 1]
-        if (j == 0) { // 特殊处理 j=0 的情况
-          alignas(32) int tmp[8] = { prech,seq[0], seq[1], seq[2],seq[3], seq[4], seq[5], seq[6] };
-          SEQ = _mm256_load_si256((reg*)tmp);
-        }
-        else {  // 直接加载连续内存 (高效)
-          const __m128i seq_chunk = _mm_loadu_si128((const __m128i*)(seq.c_str() + j - 1));
-          SEQ = _mm256_cvtepi8_epi32(seq_chunk);  // 符号扩展到32位
-        }
+      // j == 0;
+      reg Mij = k == 0 ? Neg_inf : _mm256_load_si256((reg*)(M_i));
+      reg SEQ; // seq[j - 1]
+      // 特殊处理 j=0 的情况
+      alignas(32) int tmp[8] = { char26_table['N'], seq[offset + 0], seq[offset + 1], seq[offset + 2],seq[offset + 3], seq[offset + 4], seq[offset + 5], seq[offset + 6] };
+      SEQ = _mm256_load_si256((reg*)tmp);
+
+      int* M_p = M + p * col_size;
+      // M[i][j] = std::max(M[i][j], M[p][j - 1] + mat[pre.base * para_m + seq[j - 1]]); // qsource
+      reg TMP = _mm256_setr_epi32(NEG_INF, M_p[offset + 0], M_p[offset + 1], M_p[offset + 2], M_p[offset + 3], M_p[offset + 4], M_p[offset + 5], M_p[offset + 6]); //M[p][j - 1]
+      reg mask = _mm256_cmpeq_epi32(PRE_BASE, SEQ);
+      // pre.base == seq[j - 1] ? match : mismatch
+      TMP = _mm256_add_epi32(TMP, _mm256_blendv_epi8(MISMATCH, MATCH, mask));
+      Mij = _mm256_max_epi32(Mij, TMP); //std::max(M[i][j], M[p][j - 1] + mat[pre.base * para_m + seq[j - 1]]); 
+      _mm256_store_si256((reg*)(M_i), Mij); // M[i][j] = max
+
+      for (int j = reg_size; j < col_size; j += reg_size) { // SIMD Msource
+        Mij = k == 0 ? Neg_inf : _mm256_load_si256((reg*)(M_i + j));
+        SEQ; // seq[j - 1]
+        // 直接加载连续内存 (高效)
+        const __m128i seq_chunk = _mm_loadu_si128((const __m128i*)(seq.c_str() + j - reg_size));
+        SEQ = _mm256_cvtepi8_epi32(seq_chunk);  // 符号扩展到32位
 
         int* M_p = M + p * col_size;
-        int* D_p = D + p * col_size;
         // M[i][j] = std::max(M[i][j], M[p][j - 1] + mat[pre.base * para_m + seq[j - 1]]); // qsource
-        reg TMP = j == 0 ? _mm256_setr_epi32(prev, M_p[j], M_p[j + 1], M_p[j + 2], M_p[j + 3], M_p[j + 4], M_p[j + 5], M_p[j + 6]) : _mm256_loadu_si256((reg*)(M_p + j - 1)); //M[p][j - 1]
+        TMP = _mm256_loadu_si256((reg*)(M_p + j - reg_size)); //M[p][j - 1]
 
-        reg mask = _mm256_cmpeq_epi32(PRE_BASE, SEQ);
+        mask = _mm256_cmpeq_epi32(PRE_BASE, SEQ);
         // pre.base == seq[j - 1] ? match : mismatch
         TMP = _mm256_add_epi32(TMP, _mm256_blendv_epi8(MISMATCH, MATCH, mask));
         Mij = _mm256_max_epi32(Mij, TMP); //std::max(M[i][j], M[p][j - 1] + mat[pre.base * para_m + seq[j - 1]]); 
-
+        _mm256_store_si256((reg*)(M_i + j), Mij); // M[i][j] = max
+      }
+      for (int j = 0; j < col_size; j += reg_size) { // SIMD Dsource
+        reg Dij = k == 0 ? Neg_inf : _mm256_load_si256((reg*)(D_i + j));
+        int* M_p = M + p * col_size;
+        int* D_p = D + p * col_size;
         // D[i][j] = std::max({ D[i][j], D[p][j] + e1, M[p][j] + o1 });    // dsource
         TMP = _mm256_load_si256((reg*)(D_p + j));
         Dij = _mm256_max_epi32(Dij, _mm256_add_epi32(TMP, E1)); // max(D[i][j], D[p][j] + e1)
         TMP = _mm256_load_si256((reg*)(M_p + j));
         Dij = _mm256_max_epi32(Dij, _mm256_add_epi32(TMP, O1)); // max(D[i][j], M[p][j] + o1)
-
-        _mm256_store_si256((reg*)(M_i + j), Mij); // M[i][j] = max
         _mm256_store_si256((reg*)(D_i + j), Dij); // D[i][j] = max
-        prev = M_p[j + reg_size - 1]; // pre = M[p][j +reg_size - 1]
-        prech = seq[j + reg_size - 1];// prech =seq[j + reg_size - 1]
       }
     }
-    // if (i % 100 == 0) std::cout << i << "\n";
 
     auto end2 = std::chrono::high_resolution_clock::now();
     total_part2 += std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2);
     auto start3 = std::chrono::high_resolution_clock::now();
+    // SIMD fsource
     // I[i][0] = NEG_INF;
     int* I_i = I + i * col_size;
     I_i[0] = NEG_INF;
-    for (int j = 1; j <= m; j++) {
-      I_i[j] = std::max(I_i[j - 1] + e1, M_i[j - 1] + o1); // isource
+    for (int j = 1; j < reg_size; j++) {
+      // I[i][j] = std::max(I[i][j - 1] + e1, M[i][j - 1] + o1)
+      I_i[j] = std::max(I_i[j - 1] + block_num * e1, M_i[offset + j - 1] + o1); // isource
       M_i[j] = std::max({ M_i[j], D_i[j], I_i[j] });  // three source
+    }
+    for (int j = reg_size; j < col_size; j += reg_size) {
+      // I[i][j] = std::max(I[i][j - 1] + e1, M[i][j - 1] + o1)
+      reg TMP1 = _mm256_load_si256((reg*)(I_i + j - reg_size));
+      TMP1 = _mm256_add_epi32(TMP1, E1); // max(D[i][j], D[p][j] + e1)
+      reg TMP2 = _mm256_load_si256((reg*)(M_i + j - reg_size));
+      TMP2 = _mm256_add_epi32(TMP2, O1);
+      _mm256_store_si256((reg*)(I_i + j), _mm256_max_epi32(TMP1, TMP2)); // I[i][j] = max
+
+      // M[i][j] = std::max({ M[i][j], D[i][j], I[i][j] });  // three source
+      reg Mij = _mm256_load_si256((reg*)(M_i + j));
+      TMP1 = _mm256_load_si256((reg*)(D_i + j));
+      TMP2 = _mm256_load_si256((reg*)(I_i + j));
+      Mij = _mm256_max_epi32(Mij, _mm256_max_epi32(TMP1, TMP2));
+      _mm256_store_si256((reg*)(I_i + j), Mij); // M[i][j] = max
     }
     auto end3 = std::chrono::high_resolution_clock::now();
     total_part3 += std::chrono::duration_cast<std::chrono::microseconds>(end3 - start3);
@@ -262,15 +292,20 @@ std::vector<res_t> POA_SIMD_ORIGIN(para_t* para, const graph& DAG, const std::st
   //   }
   // }
   // std::cerr << "finish" << "\n";
-  int i = n - 1, j = m;
+  int i = n - 1, j = (m % block_num) * reg_size + (m / block_num); // j = m
+  std::cerr << M[i * col_size + j] << "\n";
   std::vector<res_t> res;
   char op = 'M';
   // std::cerr << "finsh align" << "\n";
   while (i > 0 || j > 0) {
+    // std::cerr << "j: " << j << "\n";
+    int* M_i = M + i * col_size;
+    int* D_i = D + i * col_size;
+    int* I_i = I + i * col_size;
     // dsource
     if (op == 'M') {
-      if (M[i * col_size + j] == D[i * col_size + j]) op = 'D';
-      if (M[i * col_size + j] == I[i * col_size + j]) op = 'I';
+      if (M_i[j] == D_i[j]) op = 'D';
+      if (M_i[j] == I_i[j]) op = 'I';
     }
     // std::cerr << op << " " << i << " " << j << "\n";
     if (op == 'M') {
@@ -278,36 +313,37 @@ std::vector<res_t> POA_SIMD_ORIGIN(para_t* para, const graph& DAG, const std::st
       for (int k = 0; k < cur.in.size(); k++) {
         int p = node[cur.in[k]].rank; // rank
         const node_t& pre = node[cur.in[k]];
-        if (j - 1 >= 0 && M[i * col_size + j] == M[p * col_size + j - 1] + (pre.base == seq[j - 1] ? match : mismatch)) {
-          if (pre.base == seq[j - 1]) {
+        int* M_p = M + p * col_size;
+        if (j - reg_size >= 0 && M_i[j] == M_p[j - reg_size] + (pre.base == seq[j - reg_size] ? match : mismatch)) {
+          if (pre.base == seq[j - reg_size]) {
             // std::cout << "M";
             res.emplace_back(res_t(pre.id, pre.base));
           }
           else {
             // std::cout << "X";
             const node_t& par = node[pre.par_id]; // dsu.find par
-            if (par.aligned_node[seq[j - 1]] != -1) {
-              res.emplace_back(res_t(par.aligned_node[seq[j - 1]], seq[j - 1]));
+            if (par.aligned_node[seq[j - reg_size]] != -1) {
+              res.emplace_back(res_t(par.aligned_node[seq[j - reg_size]], seq[j - reg_size]));
             }
-            else res.emplace_back(res_t(-1, seq[j - 1], pre.par_id));
+            else res.emplace_back(res_t(-1, seq[j - reg_size], pre.par_id));
           }
-          i = p, j--;
+          i = p, j -= reg_size;
           break;
         }
       }
       continue;
     }
     if (op == 'I') {
-      if (j - 1 >= 0) {
+      if (j - reg_size >= 0) {
         // std::cout << "I";
-        res.emplace_back(res_t(-1, seq[j - 1]));
-        if (I[i * col_size + j] == I[i * col_size + j - 1] + e1) {
-          j--;
+        res.emplace_back(res_t(-1, seq[j - reg_size]));
+        if (I_i[j] == I_i[j - reg_size] + e1) {
+          j -= reg_size;
           continue;
         }
-        if (I[i * col_size + j] == M[i * col_size + j - 1] + o1) {
+        if (I_i[j] == M_i[j - reg_size] + o1) {
           op = 'M';
-          j--;
+          j -= reg_size;
           continue;
         }
       }
@@ -318,12 +354,14 @@ std::vector<res_t> POA_SIMD_ORIGIN(para_t* para, const graph& DAG, const std::st
       for (int k = 0; k < cur.in.size(); k++) {
         int p = node[cur.in[k]].rank; // rank
         const node_t& pre = node[cur.in[k]];
-        if (D[i * col_size + j] == D[p * col_size + j] + e1) {
+        int* D_p = M + p * col_size;
+        int* M_p = M + p * col_size;
+        if (D_i[j] == M_p[j] + o1) {
+          op = 'M';
           i = p;
           break;
         }
-        if (D[i * col_size + j] == M[p * col_size + j] + o1) {
-          op = 'M';
+        if (D_i[j] == D_p[j] + e1) {
           i = p;
           break;
         }
